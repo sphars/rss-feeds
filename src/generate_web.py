a914c38ae69c5bcff6b6fdf258df2f1038e3a454
Spencer Harston <spencer314@gmail.com>
2024-02-29 08:21:25 -0700

    web: generated webpage now has style and favicons

--- src/generate_web.py
+++ src/generate_web.py
@@ -4,30 +4,33 @@ import json
 from pathlib import Path
 from glob import glob, iglob
 
-def write_template(template_file, destination, old_string, new_string):
-    with open(template_file, "rt") as fin:
-        s = fin.read()
+def inplace_change(input_file, old_string, new_string):
+    # Safely read the input filename using 'with'
+    with open(input_file) as f:
+        s = f.read()
         if old_string not in s:
-            print(f"{old_string} not found in {template_file}")
+            print(f"{old_string} not found in {input_file}")
             return
-        with open(destination, "wt") as fout:
-            s = s.replace(old_string, new_string)
-            fout.write(s)
+
+    # Safely write the changed content, if found in the file
+    with open(input_file, 'w') as f:
+        s = s.replace(old_string, new_string)
+        f.write(s)
 
 def main():
     # copy the feeds from feeds/ directory to dist/ directory
     
     # The path to the repo root
-    path = Path(__file__).parent.parent
-
-    source = f"{path}/feeds"
-    dest = f"{path}/dist/feeds"
+    root_path = Path(__file__).parent.parent
 
     # copy the feed files from /feeds to /dist
-    shutil.copytree(source, dest, dirs_exist_ok=True)    
+    feeds_source = f"{root_path}/feeds"
+    feeds_dest = f"{root_path}/dist/feeds"
+    shutil.copytree(feeds_source, feeds_dest, dirs_exist_ok=True)    
 
+    # get the feed data from the json files
     feeds = []
-    file_list = [f for f in iglob(source + "/**/*", recursive=True) if os.path.isfile(f) and f.endswith(".json")]
+    file_list = [f for f in iglob(feeds_source + "/**/*", recursive=True) if os.path.isfile(f) and f.endswith(".json")]
     for f in file_list:
         # read the json
         with open(f, "r") as f_in:
@@ -38,17 +41,21 @@ def main():
     for feed in feeds:
         print(f"[{feed['title']}] Writing HTML...")
         table_row = f'''<tr>
-            <td><a href="{feed['link']}">{feed['title']}</a></td>
-            <td><a href="feeds/{ str(feed['title']).lower().replace(' ', '_')}/feed.xml">Feed</a></td>
-            <td>{feed["updated"]}</td>
-        </tr>'''
+                <td><a href="{feed['link']}">{feed['title']}</a></td>
+                <td><a href="feeds/{ str(feed['title']).lower().replace(' ', '_')}/feed.xml">Feed</a></td>
+                <td>{feed["updated"]}</td>
+            </tr>'''
         table_data.append(table_row)
 
-    # update the index.html file
-    index_template = f"{path}/src/index_template.html"
-    index_file = f"{path}/dist/index.html"
+    # copy everything in web to the dist
+    web_source = f"{root_path}/src/web"
+    web_dest = f"{root_path}/dist"
+    shutil.copytree(web_source, web_dest, dirs_exist_ok=True)
+
+    # update the index.html file with the table data
+    input_file = f"{root_path}/dist/index.html"
     feed_string = "\n".join(str(item) for item in table_data)
-    write_template(index_template, index_file, "<!--FEED_DATA-->", feed_string)
+    inplace_change(input_file, "<!--FEED_DATA-->", feed_string)
 
 
 if __name__ == "__main__":
